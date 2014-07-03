b545cd1300f84689109a57dfe221366a8657779e
Matt Delhey <mdelhey@tripadvisor.com>
2014-07-02 20:35:43 -0400

    Finish documentation for each of the new files

--- R/plots.R
+++ R/plots.R
@@ -1,11 +1,14 @@
-# '@export
-
+#' qhist
+#' ggplot2 implementation of hist(); uses hist()'s default binwidth calculation
+#' @export
 qhist <- function(x) {
     qplot(x, binwidth = (range(x)[2]-range(x)[1]) / nclass.Sturges(x))
 }
 
+#' ggcolor
+#' http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
+#' @export
 ggcolor <- function(n) {
-    # http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
     hues = seq(15, 375, length=n+1)
     hcl(h=hues, l=65, c=100)[1:n]
 }
--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
-# '@export
-
+#' any_na
+#' For each col in a df, see if there are any NA's in that col.
+#' @export
 any_na <- function(df, dimn = NULL) {
-    # For each col in a df, see if there are any NA's in that col.
     dimn <- ifelse(is.null(dimn), 2, dimn)
     apply(df, dimn, function(x) any(is.na(x)))
 }
