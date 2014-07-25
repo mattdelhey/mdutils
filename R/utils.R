cce64e7484a143b42682a14714f0c16ee4c69b13
Matt Delhey <mdelhey@tripadvisor.com>
2014-07-25 09:52:28 -0400

    Add percent function from stackoverflow

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,18 @@
-#' @title samp
+#' Convert proportion to percent string.
+#' @param x Proportion to convert.
+#' @param digits Number of digits to print.
+#' @param format Output format.
+#' @family utils
+#' @export
+percent <- function(x, digits = 2, format = "f", ...) {
+  paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
 #' Sample n rows from a dataframe. Works just like head/tail.
+#' @param x Dataframe to sample.
+#' @param n Number of rows to sample.
+#' @return Dataframe with n randomly selected rows.
+#' @family utils
 #' @export
 samp <- function(x, n = 6L) {
     stopifnot(length(n) == 1L)
@@ -9,8 +22,11 @@ samp <- function(x, n = 6L) {
 }
 
 
-#' @title any.na
 #' For each col in a df, see if there are any NA's in that col.
+#' @param df Dataframe to check.
+#' @param dimn Dimension to apply on. See ?apply.
+#' @return Logical vector of length = number of columns.
+#' @family utils
 #' @export
 any.na <- function(df, dimn = NULL) {
     dimn <- ifelse(is.null(dimn), 2, dimn)
@@ -18,14 +34,16 @@ any.na <- function(df, dimn = NULL) {
 }
 
 
-#' @title which.na
 #' Subset a vector to its values which are NA.
+#' @param x Vector with missing values.
+#' @return Subset of vector without missing values.
+#' @family utils
 #' @export
 which.na <- function(x) x[which(is.na(x))]
 
 
-#' @title send_email
-#' @description Wrapper for sending email using mutt.
+#' #' Wrapper for sending email using mutt.
+#' @family utils
 #' @export
 send_email <- function(to, subject, body, attachment) {
     if (!(is.character(to) && is.character(subject) && is.character(body) && is.character(attachment)))
@@ -37,7 +55,7 @@ send_email <- function(to, subject, body, attachment) {
 }
 
 
-#' #' @title last
-#' @description Get last element of object.
+#' Get last element of object.
+#' @family utils
 #' @export
 last <- function(x) head(x, n = -1)
